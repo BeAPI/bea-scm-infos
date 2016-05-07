e08bac6491fb3334e729d47cd863e006e512e190
Your Full Name <your@email.tld>
2016-05-07 14:08:13 +0200

    increment

--- bea_scm_infos.php
+++ bea_scm_infos.php
@@ -1,7 +1,7 @@
 <?php
 /*
  Plugin Name: BEA SCM Infos
- Version: 0.19.1
+ Version: 0.20
  Plugin URI: http://www.beapi.fr
  Description: Get infos from versionning system you use
  Author: BE API Technical team
@@ -24,7 +24,7 @@
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-define( 'BEA_SCM_INFOS_VERSION', '0.19' );
+define( 'BEA_SCM_INFOS_VERSION', '0.20' );
 define( 'BEA_SCM_INFOS_DIR', plugin_dir_path( __FILE__ )  );
 define( 'BEA_SCM_INFOS_URL', plugin_dir_url( __FILE__ )  );
 define( 'BEA_SCM_INFOS_VIEWS_FOLDER_NAME', 'bea-scm' );
